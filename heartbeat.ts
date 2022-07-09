7a3d119b053f16fd055dbd005266e35e0f8287ba
navigaid <navigaid@gmail.com>
2022-07-09 05:33:03 +0000

    v3: add deployment id

--- heartbeat.ts
+++ heartbeat.ts
@@ -1,14 +1,15 @@
 const DENO_REGION = Deno.env.get("DENO_REGION") ?? "localhost";
+const DENO_DEPLOYMENT_ID = Deno.env.get("DENO_DEPLOYMENT_ID") ?? '(N/A)';
 const DOMAIN = 'broadchat.deno.dev';
 
 var Counter = 0;
-const VERSION = 'v2';
+const VERSION = 'v3';
 
 async function heartbeat() {
   const url = `https://${DOMAIN}/api/send`;
   const method = "POST";
   const body = JSON.stringify({
-    user: DENO_REGION,
+    user: `${DENO_REGION}:${DENO_DEPLOYMENT_ID}`,
     body: `${VERSION} ${new Date()} ${Counter++}`,
   });
   const resp = await fetch(url, {

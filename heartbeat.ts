427d3b62fc8232ef705d87fd73ff2c84632805ba
navigaid <navigaid@gmail.com>
2022-07-09 05:42:56 +0000

    fix broxy

--- heartbeat.ts
+++ heartbeat.ts
@@ -1,9 +1,9 @@
 const DENO_REGION = Deno.env.get("DENO_REGION") ?? "localhost";
 const DENO_DEPLOYMENT_ID = Deno.env.get("DENO_DEPLOYMENT_ID") ?? '(N/A)';
-const DOMAIN = 'broadchat.deno.dev';
+const DOMAIN = 'broxy.deno.dev';
 
 var Counter = 0;
-const VERSION = 'v3';
+const VERSION = 'v4';
 
 async function heartbeat() {
   const url = `https://${DOMAIN}/api/send`;

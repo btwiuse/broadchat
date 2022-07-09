fbd2706f3cfebf26b196417494f12bacee2dda41
navigaid <navigaid@gmail.com>
2022-07-09 05:11:25 +0000

    add version and counter

--- heartbeat.ts
+++ heartbeat.ts
@@ -1,13 +1,16 @@
 const DENO_REGION = Deno.env.get("DENO_REGION") ?? "localhost";
 const DOMAIN = 'broadchat.deno.dev';
 
+var Counter = 0;
+const VERSION = 'v0';
+
 async function heartbeat() {
   // const url = "https://broxy.deno.dev/api/send";
-  const url = `https://{DOMAIN}/api/send`;
+  const url = `https://${DOMAIN}/api/send`;
   const method = "POST";
   const body = JSON.stringify({
     user: DENO_REGION,
-    body: new Date(),
+    body: `${VERSION} ${new Date()} ${Counter++}`,
   });
   const resp = await fetch(url, {
     body,
